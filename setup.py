4f4e66b1d0a13a6609b92921a7a78d720ad3e85f
Sebastian M. Ernst <ernst@pleiszenburg.de>
2023-04-15 21:35:15 +0200

    require avx2 instead of 1

--- setup.py
+++ setup.py
@@ -96,7 +96,7 @@ ext_modules = cythonize(
             "-Wdouble-promotion",
             "-Winline",
             "-Werror",
-            "-mavx",  # !!!
+            "-mavx2",
         ],
         extra_link_args=["-lm"],
     ),
