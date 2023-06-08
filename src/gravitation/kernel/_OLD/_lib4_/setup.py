777e1e80b4e5e035f94686c85ecdb542e19056d9
Sebastian M. Ernst <ernst@pleiszenburg.de>
2023-06-08 15:41:59 +0200

    todo

---
+++ src/gravitation/kernel/_OLD/_lib4_/setup.py
@@ -0,0 +1,23 @@
+
+# TODO
+
+# Extension(
+#     "gravitation.kernel._lib4_.lib",
+#     [os.path.join(SRC_DIR, "gravitation", "kernel", "_lib4_", "lib.c")],
+#     extra_compile_args=[
+#         "-std=gnu11",
+#         "-fPIC",
+#         "-O3",
+#         "-ffast-math",
+#         "-march=native",
+#         "-mtune=native",
+#         "-mfpmath=sse",
+#         "-fopenmp",
+#         "-Wall",
+#         "-Wdouble-promotion",
+#         "-Winline",
+#         "-Wno-maybe-uninitialized",
+#         "-Werror",
+#     ],
+#     extra_link_args=["-lm", "-fopenmp"],
+# ),
