d12f2c6fe08cfbb76ed89793e325ca8e70ef3725
Sebastian M. Ernst <ernst@pleiszenburg.de>
2023-06-13 01:28:27 +0200

    run on new log ingest

--- src/gravitation/cli/analyze.py
+++ src/gravitation/cli/analyze.py
@@ -28,118 +28,13 @@ specific language governing rights and limitations under the License.
 # IMPORT
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-import copy
-import json
-from typing import List
-
 import click
 
-from ..lib.debug import typechecked
-from ..lib.errors import BenchmarkLogError
+from ..lib.logsession import SessionLog
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # ROUTINES
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
-@typechecked
-def _parse_linestr_to_linedict(line: str) -> dict:
-    """parse single line from log (json str) to python dict"""
-    try:
-        return json.loads(line)
-    except json.decoder.JSONDecodeError:
-        return {"log": "JSON_ERROR", "str": line}
-
-
-@typechecked
-def _parse_itemstr_to_itemdict(raw: str) -> dict:
-    """parse log of one full benchmark worker run to one single dict"""
-
-    item = {}
-
-    lines = [
-        _parse_linestr_to_linedict(line)
-        for line in raw.split("\n")
-        if line.strip() != ""
-    ]
-
-    errors = [line for line in lines if line["log"] == "JSON_ERROR"]
-    if len(errors) != 0:
-        for idx, error in enumerate(errors, start = 1):
-            print(f"=== ERROR {idx:d} ===")
-            print(error["str"])
-        raise BenchmarkLogError("benchmark log has non-JSON components, likely errors")
-
-    errors = [line for line in lines if line["log"] == "ERROR"]
-    if len(errors) != 0:
-        for idx, error in enumerate(errors, start = 1):
-            print(f"\n=== ERROR {idx:d} ===")
-            print(error['msg'])
-        raise BenchmarkLogError("benchmark has errors")
-
-    input_ = [line for line in lines if line["log"] == "INPUT"]
-    if len(input_) > 1:
-        raise BenchmarkLogError("more than one INPUT log per benchmark worker run")
-    if len(input_) < 1:
-        raise BenchmarkLogError("INPUT log missing in benchmark worker run")
-    input_ = copy.deepcopy(input_)
-    input_[0].pop("log")
-    item["meta"] = input_[0]
-
-    size = copy.deepcopy([line for line in lines if line["log"] == "SIZE"])
-    if len(size) > 1:
-        raise BenchmarkLogError("more than one SIZE log per benchmark worker run")
-    if len(size) < 1:
-        raise BenchmarkLogError("SIZE log missing in benchmark worker run")
-    item["meta"]["simulation"]["size"] = size[0]["value"]
-
-    item["runtime"] = [line["runtime"] for line in lines if line["log"] == "STEP"]
-    item["gctime"] = [line["gctime"] for line in lines if line["log"] == "STEP"]
-
-    counter = [line["counter"] for line in lines if line["log"] == "STEP"]
-    if len(counter) == 0:
-        raise BenchmarkLogError("benchmark did not run any steps")
-    if counter != list(range(counter[0], len(counter) + counter[0])):
-        raise BenchmarkLogError("benchmark has unexpected sequence of steps")
-
-    if lines[-1] != {"log": "EXIT", "msg": "OK"}:
-        raise BenchmarkLogError("benchmark did not exit properly")
-
-    return item
-
-
-@typechecked
-def _parse_logstr_to_datalist(log: str) -> List[dict]:
-    """parse a benchmark log consisting of multiple worker runs to list of dict"""
-
-    results = []
-
-    for benchmark in log.split('{"log": "START"}\n'):
-
-        if len(benchmark.strip()) == 0:
-            continue
-
-        try:
-            result = _parse_itemstr_to_itemdict(benchmark)
-        except BenchmarkLogError as e:
-            print('-> BAD RESULT', e)
-            continue
-
-        variation = [
-            f"{key:s}={result['meta']['kernel'][key]:s}"
-            for key in sorted(result['meta']['kernel'].keys())
-        ]
-        label = " / ".join([
-            f"kernel={result['meta']['kernel']['name']:s}",
-            *variation,
-            f"implementation={result['meta']['python']['implementation']:s}",
-            f"len={result['meta']['simulation']['length']:d}",
-        ])
-
-        print(f'-> GOOD RESULT: {label:s}')
-        results.append(result)
-
-    return results
 
 
 @click.command(short_help="analyze benchmark logfile")
@@ -161,10 +56,5 @@ def _parse_logstr_to_datalist(log: str) -> List[dict]:
 )
 def analyze(logfile, data):
     """analyze benchmark logfile"""
-    data.write(
-        json.dumps(
-            _parse_logstr_to_datalist(logfile.read()),
-            indent="\t",
-            sort_keys=True,
-        )
-    )
+
+    SessionLog.ingest(logfile, data)
