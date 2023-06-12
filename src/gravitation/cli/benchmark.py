cfbe04a346064660e88d5e02da39912ea36cb844
Sebastian M. Ernst <ernst@pleiszenburg.de>
2023-06-12 15:14:59 +0200

    moved logic into class in lib

--- src/gravitation/cli/benchmark.py
+++ src/gravitation/cli/benchmark.py
@@ -28,160 +28,15 @@ specific language governing rights and limitations under the License.
 # IMPORT
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-import atexit
-from io import TextIOWrapper
-import json
-import shutil
-import sys
-from typing import Generator
-
-import termplotlib as tpl
 import click
 
-from .worker import worker_command
-
-from ..lib.baseuniverse import BaseUniverse
-from ..lib.const import Stream
-from ..lib.debug import typechecked
+from ..lib.benchmark import Benchmark
+from ..lib.const import Display
 from ..lib.kernel import KERNELS
-from ..lib.proc import run_command
-from ..lib.variation import Variation
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # ROUTINES
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
-@typechecked
-class _Processing:
-    "reading a worker log in realtime"
-
-    def __init__(
-        self,
-        kernel: str,
-        variation: Variation,
-        results: dict,
-        length: int,
-        fh: TextIOWrapper,
-        display: str,
-    ):
-        self._kernel = kernel
-        self._length = length
-        self._fh = fh
-        self._display = display
-        self._results = results
-
-        self._variation = variation
-        variation = variation.to_dict()
-        self._variation_str = ' / '.join([f'{key:s}={variation[key]:s}' for key in sorted(variation.keys())])
-
-        self._counter = 0
-
-        self._error_state = False
-        self._error = None
-
-    def __call__(
-        self,
-        stream_id: Stream,
-        line: str,
-    ):
-
-        if stream_id is Stream.stderr:
-            line = json.dumps({"log": "stderr", "value": line})
-
-        self._fh.write(f"{line:s}\n")
-        self._fh.flush()
-
-        if self._display == "log":
-            print(line)
-        if stream_id is Stream.stderr:
-            return
-
-        if not self._error_state:
-            try:
-                msg = json.loads(line)
-            except Exception as e:
-                print(line)
-                self._error_state = True
-                self._error = e
-                return
-        else:
-            print(line)
-            if line.startswith(" "):
-                return
-            raise self._error
-
-        if msg["log"] == "STEP":
-            self._counter = msg["counter"]
-
-        if msg["log"] == "BEST_TIME":
-            if self._length not in self._results.keys():
-                self._results[self._length] = msg["value"]
-            elif self._results[self._length] > msg["value"]:
-                self._results[self._length] = msg["value"]
-        else:
-            return
-
-        if self._display != "plot" or len(self._results) == 0:
-            return
-
-        x = sorted(list(self._results.keys()))
-        y = [self._results[n]*1e-9 for n in x]
-        t = shutil.get_terminal_size((80, 20))
-
-        label = " / ".join([
-            f"kernel={self._kernel:s}",
-            self._variation_str,
-            f"implementation={sys.implementation.name:s}",
-            f"len={self._length:d}",
-            f"iteration={self._counter:d}",
-            f"best={y[-1]*1e-9:.02e}s",
-        ])
-
-        fig = tpl.figure()
-        fig.plot(
-            x,
-            y,
-            label=label,
-            width=t.columns,
-            height=t.lines,
-            extra_gnuplot_arguments=[
-                "set logscale x 2",
-                'set format y "10^{%L}"',
-                "set logscale y 10",
-            ],
-        )
-        fig.show()
-
-
-@typechecked
-def _range(start: int, stop: int) -> Generator:
-    """special range generator, going from 2^start to 2^stop with some interpolation"""
-    if start > stop:
-        raise ValueError()
-    for value in range(start, stop):
-        yield 2 ** value
-        yield round(2 ** (value + 0.5))
-    yield 2 ** stop
-
-
-@typechecked
-def _common_initial_states(start: int, stop: int, datafile: int):
-    """create common initial states for benchmarks for later evaluation of results"""
-    for length in _range(start, stop):
-        print(
-            f"Creating initial state for {length:d} masses (max {2**stop:d}) ..."
-        )
-        initial_state = _UniverseZero.from_galaxy(length=length)
-        initial_state.to_hdf5(fn=datafile, gn=_UniverseZero.export_name_group(kernel = "zero", length = length, steps = 0))
-
-
-@typechecked
-class _UniverseZero(BaseUniverse):
-    "Generating common start universe"
-
-    def step_stage1(self):
-        "not required here"
 
 
 @click.command(short_help="run a benchmark across kernels")
@@ -252,7 +107,7 @@ class _UniverseZero(BaseUniverse):
 @click.option(
     "--display",
     default="plot",
-    type=click.Choice(["plot", "log", "none"]),
+    type=click.Choice([item.name for item in Display]),
     show_default=True,
     help="what to show during benchmark",
 )
@@ -275,49 +130,15 @@ def benchmark(
 ):
     """run a benchmark across kernels"""
 
-    names = sorted(list(KERNELS.keys())) if all_kernels else list(kernel)
-
-    if common_initial_state:
-        _common_initial_states(*len_range, datafile)
-
-    fh = open(logfile, "w", encoding="utf-8")
-
-    def shutdown():
-        fh.close()
-
-    atexit.register(shutdown)
-
-    for name in names:
-
-        KERNELS[name].load_meta()
-
-        for variation in KERNELS[name].variations:
-
-            if variation['threads'].name.startswith('t'):
-                continue
-
-            results = {}
-
-            for length in _range(*len_range):
-
-                run_command(
-                    worker_command(
-                        datafile=datafile,
-                        kernel=name,
-                        length=length,
-                        save_after_iteration=save_after_iteration,
-                        read_initial_state=common_initial_state,
-                        min_iterations=min_iterations,
-                        min_total_runtime=min_total_runtime,
-                        **variation.to_dict(),
-                    ),
-                    unbuffer=True,
-                    processing=_Processing(
-                        kernel=name,
-                        variation=variation,
-                        results=results,
-                        length=length,
-                        fh=fh,
-                        display=display,
-                    ),
-                )
+    Benchmark.run(
+        logfile = logfile,
+        datafile = datafile,
+        common_initial_state = common_initial_state,
+        kernels = sorted(list(KERNELS.keys())) if all_kernels else list(kernel),
+        sq_range_start = len_range[0],
+        sq_range_stop = len_range[1],
+        save_after_iteration = save_after_iteration,
+        min_iterations = min_iterations,
+        min_total_runtime = min_total_runtime,
+        display = Display[display],
+    )
